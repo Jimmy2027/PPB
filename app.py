f75392d9b319f2da138b4aa5e5b8a14961104da9
klugh <klugh@student.ethz.ch>
2020-10-05 08:57:28 +0200

    added zip option

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import configparser
+import os
 
-UPLOAD_FOLDER = 'static/uploads/'
+config_file = configparser.RawConfigParser()
+config_file.read(os.path.expanduser('~/.config/ppb.conf'))
+UPLOAD_FOLDER = config_file['ppb_config']['PPB_PATH_ON_HOST'].replace("'", '')
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.static_url_path = UPLOAD_FOLDER
--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from werkzeug.utils import secure_filename
 
 from app import app
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# todo have different folder for every file type
+#  - add an option to zip a folder and then upload it, local is done -need to do remote version
+#todo check what happens if trying to upload zip file with path
+# todo should be better to use flags in bahs like this: https://medium.com/@Drew_Stokes/bash-argument-parsing-54f3b81a6a8f
+image_extensions = set(['png', 'jpg', 'jpeg', 'gif'])
+video_extensions = set(['mov', 'mp4'])
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def check_extension(filename, extension_list):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in extension_list
 
 
 @app.route('/')
 def upload_form():
     return "<h1>Hello there</h1>"
-
-@app.route('/query_example')
-def query_example():
-    language = request.args.get('language')
-    return '<h1>The language is : {} </h1>'.format(language)
-
 
 @app.route('/file/<filename>', methods=['POST', 'GET'])
 def upload_image(filename):
@@ -31,35 +30,49 @@ def upload_image(filename):
         if filename == '':
             flash('No image selected for uploading')
             return redirect(request.url)
-        if file and allowed_file(filename):
+        elif file and check_extension(filename, image_extensions):
             filename = secure_filename(filename)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # print('upload_image filename: ' + filename)
+            print('uploading image!!!')
             return render_template('upload_image.html', filename=filename)
-        if file and filename.endswith('.pdf'):
+        elif file and filename.endswith('.pdf'):
+            print('uploading pdf!!!')
             filename = secure_filename(filename)
             return render_template('upload_pdf.html', filename=filename)
+        elif file and check_extension(filename, video_extensions):
+            filename = secure_filename(filename)
+            print('upload video!!!')
+            return render_template('upload_video.html', filename=filename)
+        elif file and filename.endswith('.zip'):
+            print('upload_zip!!!')
+            filename = secure_filename(filename)
+            return redirect(url_for('upload_image',
+                                    filename=filename))
         else:
+            print('File extention not recognized, uploading as text')
             return render_template('upload_text.html', text=file.read())
 
     if request.method == 'GET':
         if filename.endswith('.pdf'):
             filename = secure_filename(filename)
             return render_template('upload_pdf.html', filename=filename)
-        elif allowed_file(filename):
+        elif check_extension(filename, image_extensions):
+            print('getting image!!!')
             filename = secure_filename(filename)
+            print(render_template('upload_image.html', filename=filename))
             return render_template('upload_image.html', filename=filename)
+        elif check_extension(filename, video_extensions):
+            filename = secure_filename(filename)
+            return render_template('upload_video.html', filename=filename)
+        elif filename.endswith('.zip'):
+            return render_template('upload_zip.html', filename=filename)
         else:
             file = open('static/uploads/{}'.format(filename))
             return render_template('upload_text.html', text=file.read())
 
 
-
-
-
 @app.route('/display/<filename>')
 def display_image(filename):
-    # print('display_image filename: ' + filename)
+    print(filename)
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
@@ -69,7 +82,7 @@ def show_static_pdf():
         return send_file(static_file, attachment_filename='file.pdf')
 
 
-@app.route('/users/<user_id>', methods = ['GET', 'POST', 'DELETE'])
+@app.route('/users/<user_id>', methods=['GET', 'POST', 'DELETE'])
 def user(user_id):
     if request.method == 'GET':
         """return the information for <user_id>"""
@@ -80,9 +93,8 @@ def user(user_id):
         # changed to be int or whatever you want, along
         # with your lxml knowledge to make the required
         # changes
-        data = request.form.get('data') # a multidict containing POST data
+        data = request.form.get('data')  # a multidict containing POST data
         return 'The data is {}'.format(data)
-
 
 
 if __name__ == "__main__":
