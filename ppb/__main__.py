d7a431cb526185a2b91a19507c8c1fa7e17e3108
klugh <klugh@student.ethz.ch>
2021-05-18 09:35:30 +0200

    add upload function

--- ppb/__main__.py
+++ ppb/__main__.py
@@ -1,14 +1,8 @@
-# HK, 17.12.20
 import argparse
-import configparser
-import hashlib
-import os
 import sys
 from pathlib import Path
-import json
-from ppb import log
-from ppb.utils import get_unique_str
-from ppb.upload_files import upload_file_remote, upload_file_local
+
+from ppb import upload
 
 
 def main():
@@ -23,57 +17,12 @@ def main():
     parser.add_argument('--description', '-d', default='', type=str, help="Description stored in the ppb dataframe.")
     parser.add_argument('file', type=str, help='file to upload and display')
 
-    config = configparser.ConfigParser()
-    config.read(os.path.expanduser('~/.config/ppb.conf'))
-    ppb_target_host = config['ppb_config']['PPB_TARGET_HOST'].replace("'", '')
-    ppb_path_on_host = config['ppb_config']['PPB_PATH_ON_HOST'].replace("'", '')
-    ppb_http_path = config['ppb_config']['PPB_HTTP_PATH'].replace("'", '')
-    sender_name = config['other']['SENDER_NAME'].replace("'", '')
-
     flags = parser.parse_args()
 
-    if not ppb_target_host:
-        print("ERROR: The PPB_TARGET_HOST variable is unset."
-              "Nothing can be uploaded if there is no target host."
-              "Please copy the example config file from under \"/etc/ppb.conf\" to \"~/.config/ppb.conf\","
-              "and fill in the variable values according to your personal settings.")
-
-    # when running on host, ppb_http_path = ppb_path_on_host
-    if not ppb_http_path:
-        ppb_http_path = ppb_path_on_host
-
     file_path = Path(flags.file.replace(" ", "\\ "))
-    file_stem = get_unique_str(file_path.stem)
-    file_id = hashlib.md5(file_stem.encode('utf-8')).hexdigest()
-    dest_file = file_id[:12]
-    ext = file_path.suffix
 
-    # if zipping file, add .zip extension
-    dest_file += f'{ext}' + '.zip' * flags.zip_flag
-    dest_file = Path(dest_file)
-
-    in_file = dest_file
-
-    curl_command = f'curl -X POST --silent --output /dev/null --show-error ' \
-                   f'--fail {ppb_http_path}/{in_file} -d filename={dest_file} '
-
-    metadata = {
-        'lifetime': flags.lifetime,
-        'description': flags.description,
-        'orig_filename': str(file_path.stem) + ext,
-        'sender': sender_name
-    }
-
-    # send file to where it will be read by flask app
-    if ppb_target_host == 'local':
-        upload_file_local(dest_file, file_path, flags, in_file, ppb_path_on_host, metadata)
-    else:
-        upload_file_remote(dest_file, ext, file_path, flags, in_file, ppb_path_on_host, ppb_target_host,
-                           metadata=metadata)
-
-    log.info(curl_command)
-    os.system(curl_command)
-    log.info(f'Uploaded to {ppb_http_path}/{in_file}')
+    upload(file_path=file_path, zip_flag=flags.zip_flag, plain=flags.plain, description=flags.description,
+           lifetime=flags.lifetime)
 
 
 if __name__ == '__main__':
--- ppb/upload_files.py
+++ ppb/upload_files.py
@@ -1,37 +1,95 @@
-# HK, 05.01.21
+import configparser
+import hashlib
+import json
 import os
 import shutil
 import tempfile
-import zipfile
+from pathlib import Path
 
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import guess_lexer_for_filename
 from pygments.lexers.shell import BashLexer
-import json
+
 from ppb import log
 from ppb.utils import check_extension, IMAGE_EXTENSIONS, VIDEO_EXTENSIONS, COMPRESSION_EXTENSIONS
-from pathlib import Path
+from ppb.utils import get_unique_str
 
 
-def upload_file_remote(dest_file: Path, ext: str, file_path: Path, flags, in_file, ppb_path_on_host, ppb_target_host,
+def upload(file_path: Path, zip_flag: bool = False, plain: bool = False, description: str = None, lifetime: int = 20):
+    """Upload file to ppb server."""
+    config = configparser.ConfigParser()
+    config.read(os.path.expanduser('~/.config/ppb.conf'))
+    ppb_target_host = config['ppb_config']['PPB_TARGET_HOST'].replace("'", '')
+    ppb_path_on_host = config['ppb_config']['PPB_PATH_ON_HOST'].replace("'", '')
+    ppb_http_path = config['ppb_config']['PPB_HTTP_PATH'].replace("'", '')
+    sender_name = config['other']['SENDER_NAME'].replace("'", '')
+
+    if not ppb_target_host:
+        print("ERROR: The PPB_TARGET_HOST variable is unset."
+              "Nothing can be uploaded if there is no target host."
+              "Please copy the example config file from under \"/etc/ppb.conf\" to \"~/.config/ppb.conf\","
+              "and fill in the variable values according to your personal settings.")
+
+    # when running on host, ppb_http_path = ppb_path_on_host
+    if not ppb_http_path:
+        ppb_http_path = ppb_path_on_host
+
+    file_stem = get_unique_str(file_path.stem)
+    file_id = hashlib.md5(file_stem.encode('utf-8')).hexdigest()
+    dest_file = file_id[:12]
+    ext = file_path.suffix
+
+    # if zipping file, add .zip extension
+    dest_file += f'{ext}' + '.zip' * zip_flag
+    dest_file = Path(dest_file)
+
+    in_file = dest_file
+
+    file_url = f'{ppb_http_path}/{in_file}'
+    curl_command = f'curl -X POST --silent --output /dev/null --show-error ' \
+                   f'--fail {file_url} -d filename={dest_file} '
+
+    metadata = {
+        'lifetime': lifetime,
+        'description': description,
+        'orig_filename': str(file_path.stem) + ext,
+        'sender': sender_name
+    }
+
+    # send file to where it will be read by flask app
+    if ppb_target_host == 'local':
+        upload_file_local(dest_file, file_path, zip_flag, plain, in_file, ppb_path_on_host, metadata)
+    else:
+        upload_file_remote(dest_file, ext, file_path, zip_flag, plain, in_file, ppb_path_on_host, ppb_target_host,
+                           metadata=metadata)
+
+    log.info(curl_command)
+    os.system(curl_command)
+    log.info(f'Uploaded to {file_url}')
+
+    return file_url
+
+
+def upload_file_remote(dest_file: Path, ext: str, file_path: Path, zip_flag, plain, in_file, ppb_path_on_host,
+                       ppb_target_host,
                        metadata: dict) -> None:
     """
     dest_file: destination filename + extension (example: 77a80349869f.png)
-    ext: file extension
+    ext: file extension, like dest_file.suffix
     file_path: path to input file.
     """
     with tempfile.TemporaryDirectory() as tmpdirname:
         send_metadata(dest_file, metadata, ppb_path_on_host, ppb_target_host, tmpdirname)
         tempfile_path = os.path.join(tmpdirname, in_file)
-        if flags.zip_flag:
+        if zip_flag:
 
             zip_command = f'zip -r {tempfile_path} {file_path}'
             log.info(zip_command)
             os.system(zip_command)
             file_path = tempfile_path
 
-        elif not flags.plain and not check_extension(ext, {
+        elif not plain and not check_extension(ext, {
             *IMAGE_EXTENSIONS,
             *VIDEO_EXTENSIONS,
             *COMPRESSION_EXTENSIONS,
@@ -75,14 +133,14 @@ def send_metadata(dest_file, metadata: dict, ppb_path_on_host, ppb_target_host, 
     os.system(rsync_metadata_command)
 
 
-def upload_file_local(dest_file, file_path, flags, in_file, ppb_path_on_host, metadata: dict) -> None:
-    if flags.zip_flag:
+def upload_file_local(dest_file, file_path, zip_flag, plain, in_file, ppb_path_on_host, metadata: dict) -> None:
+    if zip_flag:
         zip_command = f'zip -r {ppb_path_on_host}/{in_file} {file_path}'
         log.info(zip_command)
         os.system(zip_command)
     else:
         dest_path = os.path.join(ppb_path_on_host, dest_file)
-        if flags.plain:
+        if plain:
             shutil.copyfile(file_path, dest_path)
         else:
             with open(file_path, mode='r') as c:
